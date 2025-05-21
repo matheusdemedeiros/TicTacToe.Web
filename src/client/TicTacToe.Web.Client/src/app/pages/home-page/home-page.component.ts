1059a2e432a8625b4c26e1d8d6130e1586f560b4
Matheus de Medeiros <matheus.medeiros@ndd.tech>
2025-05-21 15:59:28 -0300

    feat: cria a estrutura da página home

--- src/client/TicTacToe.Web.Client/src/app/pages/home-page/home-page.component.ts
+++ src/client/TicTacToe.Web.Client/src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatchTypes } from './models/match-types.enum';
+import { IRegisterCommand } from './models/register.model';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  public form!: FormGroup;
+  public MatchTypes = MatchTypes; // Expor enum para o template
 
+  constructor(private fb: FormBuilder) { }
+
+  public ngOnInit(): void {
+    this.form = this.fb.group({
+      fullName: ['', Validators.required],
+      nickname: ['', Validators.required],
+      matchType: [MatchTypes.CREATE, Validators.required],
+      matchId: ['']
+    });
+
+    this.form.get('matchType')?.valueChanges.subscribe(value => {
+      const matchIdControl = this.form.get('matchId');
+      if (value === MatchTypes.JOIN) {
+        matchIdControl?.setValidators([Validators.required]);
+      } else {
+        matchIdControl?.clearValidators();
+      }
+      matchIdControl?.updateValueAndValidity();
+    });
+  }
+
+  public onSubmit(): void {
+    const command: IRegisterCommand = {
+      fullName: this.form.get('fullName')!.value,
+      nickname: this.form.get('nickname')!.value,
+      matchType: this.form.get('matchType')!.value,
+      matchId: this.form.get('matchId')?.value
+    }
+  }
+
+
+  get isJoining() {
+    return this.form.get('matchType')?.value === MatchTypes.JOIN;
+  }
 }
