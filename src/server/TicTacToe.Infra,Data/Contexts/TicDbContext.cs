43558c11a901e7e16d7d8f7b5b4ef6f9444be94a
Matheus de Medeiros <matheusdemedeiros200599@gmail.com>
2025-05-22 02:16:48 -0300

    chore: cria o projeto application e altera dependencias

---
+++ src/server/TicTacToe.Infra,Data/Contexts/TicDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicTacToe.Domain.Entities.MatchModule;
+
+namespace TicTacToe.Infra_Data.Contexts
+{
+    public class TicDbContext : DbContext
+    {
+        public DbSet<TicMatch> Matches { get; set; }
+        public DbSet<TicPlayer> Players { get; set; }
+
+        public TicDbContext(DbContextOptions<TicDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(TicDbContext).Assembly);
+        }
+    }
+}
