7065609e1c22833e2925f88e5a452c7ee0df9ae7
Matheus de Medeiros <matheusdemedeiros200599@gmail.com>
2025-05-22 01:58:04 -0300

    refactor: cria as classes baseentity  e interface do repositório de partidas

---
+++ src/server/TicTacToe.Domain/Entities/BaseModule/BaseEntity.cs
@@ -0,0 +1,36 @@
+﻿namespace TicTacToe.Domain.Entities.BaseModule
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; private set; }
+        public DateTimeOffset CreatedAt { get; private set; }
+        public DateTimeOffset UpdatedAt { get; private set; }
+
+        protected BaseEntity()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTimeOffset.UtcNow;
+            UpdatedAt = DateTimeOffset.UtcNow;
+        }
+
+        protected BaseEntity(Guid id)
+        {
+            Id = id;
+            CreatedAt = DateTimeOffset.UtcNow;
+            UpdatedAt = DateTimeOffset.UtcNow;
+        }
+
+        public void Touch()
+        {
+            UpdatedAt = DateTimeOffset.UtcNow;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not BaseEntity other) return false;
+            return Id == other.Id;
+        }
+
+        public override int GetHashCode() => Id.GetHashCode();
+    }
+}
--- src/server/TicTacToe.Domain/Entities/MatchModule/TicPlayer.cs
+++ src/server/TicTacToe.Domain/Entities/MatchModule/TicPlayer.cs
@@ -1,14 +1,15 @@
-﻿using TicTacToe.Domain.SharedModule.Exceptions;
+﻿using TicTacToe.Domain.Entities.BaseModule;
+using TicTacToe.Domain.SharedModule.Exceptions;
 
 namespace TicTacToe.Domain.Entities.MatchModule
 {
-    public class TicPlayer
+    public class TicPlayer : BaseEntity
     {
         public string Name { get; private set; }
         public string NickName { get; private set; }
         public string Symbol { get; set; }
 
-        public TicPlayer(string name, string nickName, string symbol)
+        public TicPlayer(string name, string nickName, string symbol) : base()
         {
             if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(nickName))
             {
