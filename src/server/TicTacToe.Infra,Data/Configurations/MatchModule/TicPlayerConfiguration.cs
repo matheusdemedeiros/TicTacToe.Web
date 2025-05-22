5eac49197f67a323fb892487cc95b59c49dfa2be
Matheus de Medeiros <matheusdemedeiros200599@gmail.com>
2025-05-22 02:31:58 -0300

    chore: inicia mapeamento da classe TicPlayer

---
+++ src/server/TicTacToe.Infra,Data/Configurations/MatchModule/TicPlayerConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TicTacToe.Domain.Entities.MatchModule;
+
+namespace TicTacToe.Infra_Data.Configurations.MatchModule
+{
+    public class TicPlayerConfiguration : IEntityTypeConfiguration<TicPlayer>
+    {
+        public void Configure(EntityTypeBuilder<TicPlayer> builder)
+        {
+            builder.ToTable("TicPlayers");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            builder.Property(p => p.NickName)
+                .IsRequired()
+                 .HasMaxLength(150);
+        }
+    }
+}
