bd0b219ae9287bcd020fda4a806b9ac8335f092d
Matheus de Medeiros <matheusdemedeiros200599@gmail.com>
2025-05-23 14:37:48 -0300

    feat: inicia a configuração do use case createMatch

---
+++ src/server/TicTacToe.Application/UseCases/Match/CreateMatch/CreateMatchHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace TicTacToe.Application.UseCases.Match.CreateMatch
+{
+    public class CreateMatchHandler : IRequestHandler<CreateMatchCommand, CreateMatchResponse>
+    {
+        public Task<CreateMatchResponse> Handle(CreateMatchCommand request, CancellationToken cancellationToken)
+        {
+
+        }
+    }
+}
