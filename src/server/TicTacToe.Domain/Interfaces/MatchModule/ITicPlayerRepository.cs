c9a42a209ba82225fb490a3b61971042198d797d
Matheus de Medeiros <matheus.medeiros@ndd.tech>
2025-05-23 18:11:22 -0300

    feat: cria repositório de players

---
+++ src/server/TicTacToe.Domain/Interfaces/MatchModule/ITicPlayerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+using TicTacToe.Domain.Entities.MatchModule;
+
+namespace TicTacToe.Domain.Interfaces.MatchModule
+{
+    public interface ITicPlayerRepository
+    {
+        Task CreateAsync(TicPlayer player);
+        Task<TicPlayer?> RetrieveByIdAsync(Guid id);
+        Task<List<TicPlayer>> RetrieveAllAsync();
+        Task UpdateAsync(TicPlayer player);
+        Task DeleteAsync(Guid id);
+        Task<bool> HasAnyWithConditionAsync(Expression<Func<TicPlayer, bool>> condition);
+    }
+}
