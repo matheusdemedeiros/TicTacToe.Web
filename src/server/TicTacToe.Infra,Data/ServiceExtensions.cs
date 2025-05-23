4b2ec67f32c427a16b3a5d92ed5dff8e01cbcb39
Matheus de Medeiros <matheusdemedeiros200599@gmail.com>
2025-05-22 23:45:21 -0300

    refactor: adiciona e utiliza a classe ServiceExtensions

---
+++ src/server/TicTacToe.Infra,Data/ServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TicTacToe.Infra.Data.Contexts;
+
+namespace TicTacToe.Infra.Data
+{
+    public static class ServiceExtensions
+    {
+        public static void AddTicPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var ticDbConnectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<TicDbContext>(options =>
+                    options.UseSqlServer(ticDbConnectionString));
+        }
+    }
+}
