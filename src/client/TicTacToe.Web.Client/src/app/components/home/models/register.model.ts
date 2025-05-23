6c683cadcb3f340a3352e8b759fdfa1a2092ab92
Matheus de Medeiros <matheusdemedeiros200599@gmail.com>
2025-05-23 14:38:24 -0300

    refactor: inicia a configuração das interfaces no frontend

--- src/client/TicTacToe.Web.Client/src/app/components/home/models/register.model.ts
+++ src/client/TicTacToe.Web.Client/src/app/components/home/models/register.model.ts
@@ -3,6 +3,15 @@ import { MatchTypes } from "./match-types.enum";
 export interface IRegisterCommand {
     fullName: string,
     nickname: string,
+    playMode: string,
+    matchType: MatchTypes,
+    matchId?: string
+}
+
+export interface ICreateMatchCommand {
+    fullName: string,
+    nickname: string,
+    playMode: string,
     matchType: MatchTypes,
     matchId?: string
 }
