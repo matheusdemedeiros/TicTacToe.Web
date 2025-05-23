e48fbfcfd2985351888d2639adcf6356175ae44f
Matheus de Medeiros <matheusdemedeiros200599@gmail.com>
2025-05-23 14:38:56 -0300

    refactor: inicia a criação do matchcontroller na api web

---
+++ src/server/TicTacToe.WebAPI/Controllers/MatchController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TicTacToe.Infra.Data.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatchController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateMatch(int id)
+        {
+        }
+    }
+}
