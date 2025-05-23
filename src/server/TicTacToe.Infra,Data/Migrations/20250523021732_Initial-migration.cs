7c62a24fe0dc2d628f1f69a26c98a3ba2fd80f83
Matheus de Medeiros <matheusdemedeiros200599@gmail.com>
2025-05-22 23:26:37 -0300

    chore: corrige nome do projeto de infra data e adiciona a migration inicial

---
+++ src/server/TicTacToe.Infra,Data/Migrations/20250523021732_Initial-migration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicTacToe.Infra_Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TicPlayers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    NickName = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Symbol = table.Column<string>(type: "nvarchar(1)", maxLength: 1, nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TicPlayers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TicPlayers");
+        }
+    }
+}
--- src/server/TicTacToe.WebAPI/Program.cs
+++ src/server/TicTacToe.WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TicTacToe.Infra.Data.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var ticDbConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<TicDbContext>(options =>
+    options.UseSqlServer(ticDbConnectionString));
 
 var app = builder.Build();
 
