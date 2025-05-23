8c1e933643360ea431d5cb56f870b0f1f72552ee
Matheus de Medeiros <matheus.medeiros@ndd.tech>
2025-05-22 23:59:56 -0300

    feat: adiciona o roteamento no frontend

--- src/client/TicTacToe.Web.Client/src/app/app.routes.ts
+++ src/client/TicTacToe.Web.Client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { TicMatchComponent } from './components/match/tic-match/tic-match.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'ticmatch',
+        component: TicMatchComponent
+    }
+];
