1c09e06795578466f6f2e249ca49228bde41908a
Matheus de Medeiros <matheus.medeiros@ndd.tech>
2025-05-26 21:22:51 -0300

    refactor: ajusta a inserção da partida no db

--- src/server/TicTacToe.Application/UseCases/Match/CreateMatch/CreateTicMatchCommand.cs
+++ src/server/TicTacToe.Application/UseCases/Match/CreateMatch/CreateTicMatchCommand.cs
@@ -6,6 +6,6 @@ namespace TicTacToe.Application.UseCases.Match.CreateMatch
     public record CreateTicMatchCommand : IRequest<CreateTicMatchResponse>
     {
         public PlayModeType PlayMode { get; set; }
-        public Guid? InitialPlayerId { get; set; }
+        public string InitialPlayerId { get; set; }
     }
 }
