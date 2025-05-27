8f3767b02c051a7fd595df1c355aa1650f330aac
Matheus de Medeiros <matheusdemedeiros200599@gmail.com>
2025-05-27 01:47:06 -0300

    feat: cria o handler para adicionar o jogador na partida

---
+++ src/server/TicTacToe.Application/UseCases/Match/AddPlayer/AddTicPlayerPlayerResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace TicTacToe.Application.UseCases.Match.AddPlayer
+{
+    public record AddTicPlayerPlayerResponse
+    {
+        public Guid MatchId { get; internal set; }
+        public Guid PlayerId { get; internal set; }
+        public string PlayerName { get; internal set; }
+        public string Nickname { get; internal set; }
+    }
+}
---
+++ src/server/TicTacToe.Application/UseCases/Match/AddPlayer/AddTicPlayerToMatchCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace TicTacToe.Application.UseCases.Match.AddPlayer
+{
+    public record AddTicPlayerToMatchCommand : IRequest<AddTicPlayerPlayerResponse>
+    {
+        public string PlayerId { get; set; }
+        public string MatchId { get; set; }
+    }
+}
