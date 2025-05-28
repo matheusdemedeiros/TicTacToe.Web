422f04f6a9697fb2fec851acf8a3905323111229
Matheus de Medeiros <matheusdemedeiros200599@gmail.com>
2025-05-28 14:09:47 -0300

    feat: cria os fluxos de criação e junção de partida

--- src/client/TicTacToe.Web.Client/src/app/components/home/shared/components/register-flow/register-flow.component.ts
+++ src/client/TicTacToe.Web.Client/src/app/components/home/shared/components/register-flow/register-flow.component.ts
@@ -1,8 +1,9 @@
 import { Component, inject, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
-import { take } from 'rxjs';
+import { EMPTY, mergeMap, Observable, of } from 'rxjs';
 
 import { StepOneComponent } from './steps/step-one/step-one.component';
 import { StepTwoComponent } from './steps/step-two/step-two.component';
@@ -32,11 +33,16 @@ export class RegisterFlowComponent implements OnDestroy {
   private fb: FormBuilder;
   private playerService: PlayerService;
   private matchService: MatchService;
+  private ticPlayerId: string = '';
+  private ticMatchId: string = '';
+  private router: Router;
+  private successRoute: string = 'ticmatch'
 
   constructor() {
     this.fb = inject(FormBuilder);
     this.playerService = inject(PlayerService);
     this.matchService = inject(MatchService);
+    this.router = inject(Router);
     this.form = this.fb.group({
       fullName: ['', Validators.required],
       nickname: ['', Validators.required],
@@ -50,77 +56,36 @@ export class RegisterFlowComponent implements OnDestroy {
     this.currentStepNumber = this.multiStepFormManager.currentStepNumber;
   }
 
-  public onNextStep(): void {
-    console.log(this.form)
+  public get fullNameControl(): FormControl {
+    return this.form.get('fullName') as FormControl;
+  }
+
+  public get nickNameControl(): FormControl {
+    return this.form.get('nickname') as FormControl;
+  }
+
+  public get playModeControl(): FormControl {
+    return this.form.get('playMode') as FormControl;
+  }
+
+  public get matchTypeControl(): FormControl {
+    return this.form.get('matchType') as FormControl;
+  }
+
+  public get matchIdControl(): FormControl {
+    return this.form.get('matchId') as FormControl;
+  }
+
+  protected onNextStep(): void {
     this.updateSteps();
   }
 
-  public onPreviousStep(): void {
+  protected onPreviousStep(): void {
     this.updateSteps(true);
   }
 
-  public onCompleteForm(): void {
-    // this.createPlayer();
-    this.createMatch();
-  }
-
-  public createPlayer(): void {
-    const createPlayerCommand: ICreateTicPlayerCommand = {
-      name: this.form.get('fullName')!.value,
-      nickName: this.form.get('nickname')!.value
-    }
-
-    this.playerService.create(createPlayerCommand)
-      .pipe(take(1))
-      .subscribe({
-        next: (response: ICreateTicPlayerResponse) => {
-          console.log('response', response);
-        },
-        error: (error: any) => {
-          console.log('error', error);
-        },
-      });
-    ;
-  }
-
-  public createMatch(initialPlayerId: string = ''): void {
-    const createMatchCommand: ICreateTicMatchCommand = {
-      playMode: this.form.get('playMode')!.value,
-      initialPlayerId: initialPlayerId
-    }
-
-    this.matchService.create(createMatchCommand)
-      .pipe(take(1))
-      .subscribe({
-        next: (response: ICreateTicMatchResponse) => {
-          console.log('response', response);
-        },
-        error: (error: any) => {
-          console.log('error', error);
-        },
-      });
-    ;
-  }
-
-  public addPlayer(): void {
-    const playerId = '';
-    const matchId = this.form.get('matchId')!.value;
-    const addPlayerCommand: IAddTicPlayerToMatchCommand = {
-      matchId: matchId,
-      playerId: playerId
-    }
-
-    this.matchService.addPlayer(addPlayerCommand, matchId)
-      .pipe(take(1))
-      .subscribe({
-        next: (response: IAddTicPlayerToMatchResponse) => {
-          console.log('response', response);
-        },
-        error: (error: any) => {
-          console.log('error', error);
-        },
-      });
-    ;
+  protected onCompleteForm(): void {
+    this.handleMatch()
   }
 
   public ngOnDestroy(): void {
@@ -136,4 +101,112 @@ export class RegisterFlowComponent implements OnDestroy {
     this.currentFormStep = this.multiStepFormManager.currentFormStep;
     this.currentStepNumber = this.multiStepFormManager.currentStepNumber;
   }
-} 
+
+  private createPlayer(): Observable<ICreateTicPlayerResponse> {
+    const createPlayerCommand: ICreateTicPlayerCommand = {
+      name: this.fullNameControl.value,
+      nickName: this.nickNameControl.value
+    }
+
+    return this.playerService.create(createPlayerCommand);
+  }
+
+  private createMatch(initialPlayerId: string = ''): Observable<ICreateTicMatchResponse> {
+    const createMatchCommand: ICreateTicMatchCommand = {
+      playMode: this.playModeControl.value,
+      initialPlayerId: initialPlayerId
+    }
+
+    return this.matchService.create(createMatchCommand);
+  }
+
+  private addPlayer(matchId: string = ''): Observable<IAddTicPlayerToMatchResponse> {
+    const addPlayerCommand: IAddTicPlayerToMatchCommand = {
+      matchId: matchId ?? this.ticMatchId,
+      playerId: this.ticPlayerId
+    }
+
+    return this.matchService.addPlayer(addPlayerCommand, this.ticMatchId);
+  }
+
+  private handleMatch(): void {
+    switch (this.matchTypeControl.value) {
+      case MatchTypes.CREATE:
+        this.createMatchFlow();
+        break;
+      case MatchTypes.JOIN:
+        this.joinMatchFlow();
+        break;
+      default:
+        break;
+    }
+  }
+
+  private createMatchFlow(): void {
+    this.createPlayer()
+      .pipe(
+        mergeMap((createTicPlayerResponse: ICreateTicPlayerResponse) => {
+          if (!createTicPlayerResponse) {
+            return EMPTY;
+          }
+
+          this.ticPlayerId = createTicPlayerResponse.id;
+          return this.createMatch(this.ticPlayerId)
+        }),
+        mergeMap((createTicMatchResponse: ICreateTicMatchResponse) => {
+          if (!createTicMatchResponse) {
+            return EMPTY;
+          }
+
+          this.ticMatchId = createTicMatchResponse.matchId;
+          return of(true);
+        }))
+      .subscribe({
+        next: (response: any) => {
+
+          console.log('success', response)
+          this.router.navigate([this.successRoute])
+        },
+        error: (error: any) => console.log('error', error)
+      });
+  }
+
+  private joinMatchFlow(): void {
+    this.createPlayer()
+      .pipe(
+        mergeMap((createTicPlayerResponse: ICreateTicPlayerResponse) => {
+          if (!createTicPlayerResponse) {
+            return EMPTY;
+          }
+
+          this.ticPlayerId = createTicPlayerResponse.id;
+          const matchId = this.matchIdControl.value;
+          return this.addPlayer(matchId);
+        }))
+      .subscribe({
+        next: (response: any) => {
+          debugger;
+          console.log('success', response)
+          this.router.navigate([this.successRoute])
+        },
+        error: (error: Error) => console.log('error', error)
+      });
+  }
+
+  /*
+  1 - criação do player;
+  2 - escolher o playmode;
+  3 - criar a partida;
+  4 - adicionar o player na partida;
+  (frontend é redirecionado para a tela do tabuleiro).
+  5 - Iniciar a partida
+  5.a - Se for PlayerVsPlayer => fazer o gerenciamento com o signalr;
+    (o segundo player precisa incluir o id da partida na tela e entrar nela);
+    A partir deste momento, o tabuleiro será desbloqueado em ambas as telas,
+    permitindo que sejam feitas as chamadas de movimento.
+  5.b - Se for PlayerVsComputer => a cada movimento do usuário, deve ser gerado 
+  um movimento randômico e válido para a partida no backend; Essa informação será
+  retornada para a tela (talvez utilizando a técnica de pooling);
+
+  */
+}
