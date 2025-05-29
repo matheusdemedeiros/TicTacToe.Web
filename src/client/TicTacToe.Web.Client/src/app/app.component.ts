d0e9768ff6541881b38af83d75dbe99d5bb91684
Matheus de Medeiros <matheus.medeiros@ndd.tech>
2025-05-29 19:59:37 -0300

    refactor: remove código de teste da classe app

--- src/client/TicTacToe.Web.Client/src/app/app.component.ts
+++ src/client/TicTacToe.Web.Client/src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TicMatchHubService } from './components/match/shared/services/tic-match-hub.service';
 
 @Component({
   selector: 'app-root',
@@ -10,19 +9,4 @@ import { TicMatchHubService } from './components/match/shared/services/tic-match
 })
 export class AppComponent {
   title = 'TicTacToe';
-
-  constructor() {
-    const service = inject(TicMatchHubService)
-    service.connect('E12294C5-AFCE-4C39-A437-4C7031072C64')
-  .then(() => {
-    if (service['hubConnection']?.state === 'Connected') {
-      service.joinMatch('E12294C5-AFCE-4C39-A437-4C7031072C64');
-      console.log('Conectado e joinMatch chamado');
-    } else {
-      console.warn('Conexão não estabelecida');
-    }
-  })
-  .catch(err => console.error('Erro ao conectar ao SignalR', err));
-
-  }
 }
