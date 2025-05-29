cbec8f91cf6f37e4d158b2d3a6c62a9b77fe8532
Matheus de Medeiros <matheus.medeiros@ndd.tech>
2025-05-29 20:04:29 -0300

    refactor: conecta no grupo da partida e recebe o evento de player adicionado

--- src/client/TicTacToe.Web.Client/src/app/components/match/tic-match/tic-match.component.ts
+++ src/client/TicTacToe.Web.Client/src/app/components/match/tic-match/tic-match.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { TicBoardComponent } from '../tic-board/tic-board.component';
+import { TicMatchHubService } from '../shared/services/tic-match-hub.service';
 
 @Component({
   selector: 'app-tic-match',
@@ -16,13 +17,38 @@ export class TicMatchComponent implements OnInit {
   protected currentPlayerId: string = '';
 
   private route: ActivatedRoute;
+  private ticMatchHubService: TicMatchHubService;
 
   constructor() {
     this.route = inject(ActivatedRoute);
+    this.ticMatchHubService = inject(TicMatchHubService)
   }
 
   public ngOnInit(): void {
     this.currentMatchId = this.route.snapshot.queryParamMap.get('ticMatchId')!;
     this.currentPlayerId = this.route.snapshot.queryParamMap.get('ticPlayerId')!;
+
+    this.ticMatchHubService.connectionEstablished
+      .subscribe({
+        next: (response: boolean) => {
+          if (response) {
+            this.connectToMatchHub();
+            this.onPlayerJoined();
+          }
+        }
+      })
+  }
+
+  private connectToMatchHub(): void {
+    this.ticMatchHubService.joinMatch(this.currentMatchId);
+    console.log('Conectado e joinMatch chamado');
+  }
+
+  private onPlayerJoined(): void {
+    this.ticMatchHubService.onPlayerJoined().subscribe({
+      next: (response: any) => {
+        console.log('response do tic event', response)
+      }
+    })
   }
 }
