758aa3e64b90a4c92afdbb0db1d19b5040721943
Matheus de Medeiros <matheusdemedeiros200599@gmail.com>
2025-05-29 20:04:29 -0300

    refactor: conecta no grupo da partida e recebe o evento de player adicionado

--- src/client/TicTacToe.Web.Client/src/app/components/match/shared/services/tic-match-hub.service.ts
+++ src/client/TicTacToe.Web.Client/src/app/components/match/shared/services/tic-match-hub.service.ts
@@ -1,30 +1,28 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { environment } from '../../../../../environments/environment';
 
-import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { Observable } from 'rxjs';
+
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 
 @Injectable({ providedIn: 'root' })
 export class TicMatchHubService {
-  private hubConnection?: HubConnection;
+  public connectionEstablished = new EventEmitter<Boolean>();
+
   private readonly hubUrl = `${environment.apiUrl}Ticmatchhub`;
+  private hubConnection?: HubConnection;
+  private connectionIsEstablished = false;
 
-  public connect(matchId: string): Promise<void> {
-    if (this.hubConnection?.state === 'Connected') return Promise.resolve();
-
-    this.hubConnection = new HubConnectionBuilder()
-      .withUrl(`${this.hubUrl}?matchId=${matchId}`)
-      .build();
-
-    return this.hubConnection.start();
+  constructor() {
+    this.createConnection();
+    this.startConnection();
   }
 
   public joinMatch(matchId: string): void {
     this.hubConnection!.invoke('JoinMatchAsync', matchId);
-
   }
 
-  public listenPlayerJoined(): Observable<any> {
+  public onPlayerJoined(): Observable<any> {
     return new Observable((observer) => {
       this.hubConnection?.on('TicPlayerJoined', (match) => {
         observer.next(match);
@@ -34,5 +32,25 @@ export class TicMatchHubService {
 
   public disconnect(): Promise<void> {
     return this.hubConnection?.stop() ?? Promise.resolve();
+  }
+
+  private createConnection() {
+    this.hubConnection = new HubConnectionBuilder()
+      .withUrl(this.hubUrl)
+      .build();
+  }
+
+  private startConnection(): void {
+    this.hubConnection!
+      .start()
+      .then(() => {
+        this.connectionIsEstablished = true;
+        console.log('Hub connection started');
+        this.connectionEstablished.emit(true);
+      })
+      .catch(err => {
+        console.log('Error while establishing connection, retrying...');
+        setTimeout(() => { this.startConnection(); }, 5000);
+      });
   }
 }
