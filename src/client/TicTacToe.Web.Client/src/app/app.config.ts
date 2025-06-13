be5e6ab56d0ea863c73b9271cf125ce14ec8df95
Matheus de Medeiros <matheus.medeiros@ndd.tech>
2025-06-13 18:28:34 -0300

    feat: cria serviço de toastr no projeto

--- src/client/TicTacToe.Web.Client/src/app/app.config.ts
+++ src/client/TicTacToe.Web.Client/src/app/app.config.ts
@@ -1,13 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideAnimations(),
+    provideToastr({ preventDuplicates: true, timeOut: 900 }),
   ]
 };
