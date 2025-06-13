f3d8fb7c099288a9510bc190ba32e991c4c72a5a
Matheus de Medeiros <matheusdemedeiros200599@gmail.com>
2025-06-13 14:49:53 -0300

    feat: adiciona validações no domínio

--- src/client/TicTacToe.Web.Client/src/app/components/home/shared/components/register-flow/register-flow.component.ts
+++ src/client/TicTacToe.Web.Client/src/app/components/home/shared/components/register-flow/register-flow.component.ts
@@ -151,6 +151,7 @@ export class RegisterFlowComponent implements OnDestroy {
           }
 
           this.ticPlayerId = createTicPlayerResponse.id;
+          
           return this.createMatch(this.ticPlayerId)
         }),
         mergeMap((createTicMatchResponse: ICreateTicMatchResponse) => {
